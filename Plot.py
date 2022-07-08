26b11c4e8c776d6df05100c911f9d7396facf0fe
Taylor Roper <taylorroper@pop-os.localdomain>
2022-07-07 21:03:18 -0600

    Edited splines script, splines notebook, and Plot script to clean up code and adjust for new data. Edited gamma_fitting notebook to explore using moments.

--- Plot.py
+++ Plot.py
@@ -1,3 +1,7 @@
+month = 'wet'
+level = 50
+plot_errors = False
+
 import numpy as np
 import pygrib
 
@@ -6,83 +10,115 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 from mpl_toolkits.basemap import Basemap, cm
 
+# read in grib file
 
-fn_grb = 'blend.t00z.qmd.f012.co.grib2'
+if month == 'jan':
+    date = '20220111'
+    lead_time = '00'
+    forecast_time = '018'
+elif month == 'march':
+    date = '20220324'
+    lead_time = '12'
+    forecast_time = '018'
+elif month == 'april':
+    date = '20220425'
+    lead_time = '12'
+    forecast_time = '018'
+elif month == 'july':
+    date = '20220703'
+    lead_time = '00'
+    forecast_time = '018'
+elif month == 'sept':
+    date = '20210920'
+    lead_time = '12'
+    forecast_time = '018'
+elif month == 'oct':
+    date = '20211015'
+    lead_time = '00'
+    forecast_time = '018'
+elif month == 'dec':
+    date = '20211205'
+    lead_time = '12'
+    forecast_time = '018'
+elif month == 'wet':
+    date = ''
+    lead_time = '00'
+    forecast_time = '012'
+else:
+    print('Issue with month input.')
+fn_grb = 'data/blend' + date + '.t' + lead_time + 'z.qmd.f' + forecast_time + '.co.grib2'
 ds_grb = pygrib.open(fn_grb)
+
+# latitude and longitude grid values
 lat, long = ds_grb.message(2).data()[1:]
+
+# extracting data
 precip_shape = lat.shape
-precip_levels = np.zeros(shape=(99,)+precip_shape)
+precip = np.zeros(shape=(99,)+precip_shape)
 for i in range(99):
-    precip_levels[i,:,:] = ds_grb.message(i+2).data()[0]
+    precip[i,:,:] = ds_grb.message(i+2).data()[0] # ACPC:surface:12-18 hour acc fcst
+ds_grb.close()
 
-precip_levels_approx_unif = np.load('precip_levels_approx_unif.npy')
-precip_levels_approx_var = np.load('precip_levels_approx_var_.npy')
+precip_unif = np.load('results/precip_unif_' + month, allow_pickle=True)
+precip_var = np.load('results/precip_var_' + month, allow_pickle=True)
+errors_unif = np.load('results/errors_unif_' + month, allow_pickle=True)
+errors_var = np.load('results/errors_var_' + month, allow_pickle=True) 
 
-class Plot:
 
-    def __init__(self):
-        self.lat = lat
-        self.long = long
-        self.orig = precip_levels
-        self.approx_unif = precip_levels_approx_unif
-        self.approx_var = precip_levels_approx_var
+def Basemap_plot(data, long, lat, diff=False, name=None, color_label='mm of precipiation'):
+                
+    map = Basemap(llcrnrlon=-123.,llcrnrlat=20., 
+                urcrnrlon=-59., urcrnrlat=48., 
+                projection='lcc', 
+                lat_1=38.5,
+                lat_0=38.5,
+                lon_0=-97.5,
+                resolution='l')
 
-    def Basemap_plot(self, level, diff, unif, scale=1):
+    # draw coastlines, country boundaries, fill continents
+    map.drawcoastlines(linewidth=0.25)
+    map.drawcountries(linewidth=0.25)
+    map.fillcontinents(color='xkcd:white',lake_color='xkcd:white')
 
-        data = self.orig[level-1,:,:]
 
-        possible_levels = np.array([30, 60, 90])
-        level_idx = np.where(possible_levels == level)[0][0]
-        if diff:
-            if unif:
-                data = self.approx_unif[level_idx,:,:]-data
-                var_name = f'Uniform node error at {level}% level'
-            else:
-                data = self.approx_var[level_idx,:,:]-data
-                var_name = f'Variable node error at {level}% level'
-        else:
-            var_name = f'Precipiation at probability level {level}%'
 
+    # draw the edge of the map projection region (the projection limb)
+    map.drawmapboundary(fill_color='xkcd:white')
+    map.drawstates()
+
+    # draw lat/lon grid lines every 30 degrees.
+    map.drawmeridians(np.arange(-180,180,30))
+    map.drawparallels(np.arange(-90,90,30))
+
+    x, y = map(long, lat)
+
+    if diff:
+        boundary = int(np.ceil(max(np.abs(data.min()), np.abs(data.max()))))
+        levels = list(range(-boundary, boundary+1))
+        plt.pcolormesh(x, y, data,
+                    norm=colors.Normalize(vmin=levels[0], vmax=levels[-1]),
+                    cmap='seismic', shading='nearest')
+        # map.contourf(x, y, data, 16, levels=levels, cmap='seismic')
+        map.colorbar()
+        map.colorbar().set_label(color_label)
+    else:
+        map.contourf(x, y, data, 16, linewidths=1.5)
+        map.colorbar()
+        map.colorbar().set_label(color_label)
         
-        map = Basemap(llcrnrlon=-123.,llcrnrlat=20., 
-                    urcrnrlon=-59., urcrnrlat=48., 
-                    projection='lcc', 
-                    lat_1=38.5,
-                    lat_0=38.5,
-                    lon_0=-97.5,
-                    resolution='l')
+    if name is not None:
+        plt.title(name)
+        
+    plt.show()
 
-        # draw coastlines, country boundaries, fill continents
-        map.drawcoastlines(linewidth=0.25)
-        map.drawcountries(linewidth=0.25)
-        map.fillcontinents(color='xkcd:white',lake_color='xkcd:white')
-
-
-
-        # draw the edge of the map projection region (the projection limb)
-        map.drawmapboundary(fill_color='xkcd:white')
-        map.drawstates()
-
-        # draw lat/lon grid lines every 30 degrees.
-        map.drawmeridians(np.arange(-180,180,30))
-        map.drawparallels(np.arange(-90,90,30))
-
-        x, y = map(self.long, self.lat)
-
-        if diff:
-            if scale ==0:
-                data_abs_max = int(np.ceil(max(np.abs(data.min()),np.abs(data.max()))))
-                levels = list(range(-data_abs_max,data_abs_max+1))
-            else:
-                levels = list(range(-scale, scale+1))
-            plt.pcolormesh(x, y, data,
-                        norm=colors.Normalize(vmin=levels[0], vmax=levels[-1]),
-                        cmap='seismic', shading='nearest')
-            # map.contourf(x, y, data, 16, levels=levels, cmap='seismic')
-            map.colorbar()
-            map.colorbar().set_label('mm of precipitation')
-        else:
-            map.contourf(x, y, data, 16, linewidths=1.5)
-
-        plt.title(var_name)
-        plt.show()
+level_idx = np.where(np.array([5,25,50,75,95]) == level)[0]
+Basemap_plot(data=precip[level-1,:,:], long=long, lat=lat, name=f'Precipitation at {level}% quantile')
+Basemap_plot(data=precip_unif[level-1,:,:]-precip[level-1,:,:], long=long, lat=lat, diff=True, name=f'Uniform node error at {level}% quantile')
+Basemap_plot(data=precip_var[level-1,:,:]-precip[level-1,:,:], long=long, lat=lat, diff=True, name=f'Variable node error at {level}% quantile')
+if plot_errors:
+    Basemap_plot(data=errors_unif[0,:,:], long=long, lat=lat, name=f'KS statistic for uniform nodes at {level}% quantile', color_label='KS')
+    Basemap_plot(data=errors_unif[1,:,:], long=long, lat=lat, name=f'L_1 norm for uniform nodes at {level}% quantile', color_label='L_1 norm')
+    Basemap_plot(data=errors_unif[2,:,:], long=long, lat=lat, name=f'CRPS for uniform nodes at {level}% quantile', color_label='CRPS')
+    Basemap_plot(data=errors_var[0,:,:], long=long, lat=lat, name=f'KS statistic for variable nodes at {level}% quantile', color_label='KS')
+    Basemap_plot(data=errors_var[1,:,:], long=long, lat=lat, name=f'L_1 norm for variable nodes at {level}% quantile', color_label='L_1 norm')
+    Basemap_plot(data=errors_var[2,:,:], long=long, lat=lat, name=f'CRPS for variable nodes at {level}% quantile', color_label='CRPS')
--- splines.py
+++ splines.py
@@ -1,21 +1,102 @@
+
+approx_type = 'unif' # 'unif', 'var', or 'both'
+error_calc = True
+save = True
+month = 'wet'
+
 import numpy as np
 import pygrib
-from scipy import interpolate, optimize 
-import multiprocessing as mp
+from scipy import optimize
+
 import time
+
+# read in grib file
+
+if month == 'jan':
+    date = '20220111'
+    lead_time = '00'
+    forecast_time = '018'
+elif month == 'march':
+    date = '20220324'
+    lead_time = '12'
+    forecast_time = '018'
+elif month == 'april':
+    date = '20220425'
+    lead_time = '12'
+    forecast_time = '018'
+elif month == 'july':
+    date = '20220703'
+    lead_time = '00'
+    forecast_time = '018'
+elif month == 'sept':
+    date = '20210920'
+    lead_time = '12'
+    forecast_time = '018'
+elif month == 'oct':
+    date = '20211015'
+    lead_time = '00'
+    forecast_time = '018'
+elif month == 'dec':
+    date = '20211205'
+    lead_time = '12'
+    forecast_time = '018'
+elif month == 'wet':
+    date = ''
+    lead_time = '00'
+    forecast_time = '012'
+else:
+    print('Issue with month input.')
+fn_grb = 'data/blend' + date + '.t' + lead_time + 'z.qmd.f' + forecast_time + '.co.grib2'
+ds_grb = pygrib.open(fn_grb)
+
+# latitude and longitude grid values
+lat, long = ds_grb.message(2).data()[1:]
+
+# extracting data
+precip_shape = lat.shape
+precip = np.zeros(shape=(99,)+precip_shape)
+for i in range(99):
+    precip[i,:,:] = ds_grb.message(i+2).data()[0] # ACPC:surface:12-18 hour acc fcst
+ds_grb.close()
+
+read_temp = False
+if read_temp:
+    ds_grb = pygrib.open(fn_grb)
+    temp_shape = lat.shape
+    temp = np.zeros(shape=(99,)+temp_shape)
+    for i in range(99):
+        temp[i,:,:] = ds_grb.message(i+215).data()[0] # TMP:2 m above ground:0-18 hour max
+    ds_grb.close()
+
+# masking precip at grid points that are not monotonic
+mask = np.zeros(precip.shape)
+for i in range(lat.shape[0]):
+    for j in range(lat.shape[1]):
+        issue = False
+        for level in range(99-1):
+            if precip[level,i,j] > precip[level+1,i,j]:
+                if not issue:
+                    issue = True
+                    #mask[level+1:,i,j] = np.ones(mask.shape[0] - level - 1)
+                    mask[:,i,j] = np.ones(mask.shape[0])
+                    
+precip = np.ma.masked_array(precip, mask)
 
 # linear spline functions
 
-def linear_splines_unif(data, num_knots=10, level_width=1, zero_inflated=True):   
+def linear_splines_unif(data, num_knots=10, zero_inflated=True):   
     '''
     Calculates piecewise linear splines for quantile data using specified number of 
     knots uniformly spaced and returning interpolated approximation at every 
-    quantile level with level_width.
+    quantile level.
     ''' 
+
+    # checking if cdf is all zero
+    if data[-1] == 0:
+        return np.zeros(99)
     
     if zero_inflated:
         # calculating where cdf starts being nonzero (all zero cdf's should not be inputted)
-        levels = np.array(range(1,100))
         knot_ = np.where(data > 0)[0].min() - 1   
         if knot_ > 1:
             knots = np.unique(np.linspace(knot_-1, 98, num_knots-1, dtype=int))
@@ -25,9 +106,8 @@ def linear_splines_unif(data, num_knots=10, level_width=1, zero_inflated=True):
     else:
         knots = np.unique(np.linspace(0, 98, num_knots, dtype=int))
         
-    # approx = interpolate.interp1d(knots+1, data[knots], assume_sorted=True) 
-    # return approx(levels[level_width-1::level_width])
-    return np.interp(levels[level_width-1::level_width], knots+1, data[knots])
+    levels = range(1,100)
+    return np.interp(levels, knots+1, data[knots])
 
 def linear_splines(x, num_knots, *params):
     '''
@@ -38,7 +118,7 @@ def linear_splines(x, num_knots, *params):
     knots = list(params[0][num_knots:])
     return np.interp(x, knots, knot_vals)
 
-def linear_splines_var(data, num_knots=10, level_width=1, zero_inflated=True):
+def linear_splines_var(data, num_knots=5, zero_inflated=True):
     '''
     Calculates piecewise linear splines for quantile data using specified number of
     knots with optimized placement and returning interpolated approximation at every
@@ -47,7 +127,7 @@ def linear_splines_var(data, num_knots=10, level_width=1, zero_inflated=True):
     
     # checking if cdf is all zero
     if data[-1] == 0:
-        return np.zeros(int(np.floor(100/level_width)))
+        return np.zeros(99)
 
     # setting up intial value of parameters
     p_0 = np.linspace(0,98,num_knots).astype(int)
@@ -58,52 +138,79 @@ def linear_splines_var(data, num_knots=10, level_width=1, zero_inflated=True):
     try:
         fit, _ = optimize.curve_fit(lambda x, *params : linear_splines(x, num_knots, params), 
                 np.linspace(1,99,99), data, p_0)
-        levels = np.linspace(1,99,99)
-        levels = levels[level_width-1::level_width]
+        levels = range(1,100)
         return np.interp(levels, fit[num_knots:], fit[:num_knots])
     except RuntimeError:
-        return linear_splines_unif(data, num_knots, level_width, zero_inflated)
+        return linear_splines_unif(data, num_knots*2, zero_inflated)
+    
+qs = np.linspace(0.01, 0.99, 99)
+N = int(1e3)
 
-# read in grib file
-fn_grb = 'blend.t00z.qmd.f012.co.grib2'
-ds_grb = pygrib.open(fn_grb)
+def calc_errors(orig, approx):
+    xs = np.linspace(orig.min(), orig.max(), N)
+    differences = np.abs(np.interp(xs[1:], orig, qs) - np.interp(xs[1:], approx, qs))
+    differences_weighted = differences * (xs[1:] - xs[:-1])
+    return [differences.max(), np.sum(differences_weighted), np.sum(differences * differences_weighted)]
 
-# latitude and longitude grid values
-lat, long = ds_grb.message(2).data()[1:]
 
-# extracting precipitation levels for 6 hr forecast
-precip_shape = lat.shape
-global precip_levels
-precip_levels = np.zeros(shape=(99,)+precip_shape)
-for i in range(99):
-    precip_levels[i,:,:] = ds_grb.message(i+2).data()[0]
+precip_unif = np.zeros(shape=(99,)+lat.shape)
+precip_var = np.zeros(shape=(99,)+lat.shape)
+if error_calc:
+    errors_unif = np.zeros((3,) + lat.shape)
+    errors_var = np.zeros((3,) + lat.shape)
+idx = np.where(mask[-1,:,:] == 0)
+level_idxs = [4, 24, 50, 74, 94]
 
-# initializing output
-level_width = 30 # level_width defined here, but any updates should be manually made in wrap function
-global precip_levels_approx_var
-precip_levels_approx_var = np.zeros(shape=(int(np.floor(100/level_width)),)+lat.shape)
-global nonzero_idx
-nonzero_idx = np.where(precip_levels[-1,:,:] != 0)
+if approx_type == 'unif':
+    for n in range(idx[0].shape[0]):
+        i = idx[0][n]
+        j = idx[1][n]
+        if precip[-1,i,j] != 0:
+            precip_unif[:,i,j] = linear_splines_unif(data=precip[:,i,j], num_knots=10, zero_inflated=True)  
+            if error_calc:
+                errors_unif[:,i,j] = calc_errors(precip[:,i,j], precip_unif[:,i,j])
+    precip_unif = np.ma.masked_array(precip_unif, mask)
+    errors_unif = np.ma.masked_array(errors_unif, [mask[-1,:,:], mask[-1,:,:], mask[-1,:,:]])
+    if save:
+        precip_unif[level_idxs,:,:].dump('results/precip_unif_' + month)
+        errors_unif.dump('results/errors_unif_' + month)
 
-# wrapped function for parallel processing
-def wrap(n):
-    i = nonzero_idx[0][n]
-    j = nonzero_idx[1][n]
-    precip_levels_approx_var[:,i,j] = linear_splines_var(precip_levels[:,i,j], 10, 30) # 10 = num_knots; 30 = level_width
+elif approx_type == 'var':
+    for n in range(idx[0].shape[0]):
+        i = idx[0][n]
+        j = idx[1][n]
+        if precip[-1,i,j] != 0:
+            precip_var[:,i,j] = linear_splines_var(data=precip[:,i,j], num_knots=10, zero_inflated=True) 
+            if error_calc:
+                errors_var[:,i,j] = calc_errors(precip[:,i,j], precip_var[:,i,j])
+    precip_var = np.ma.masked_array(precip_var, mask)
+    errors_var = np.ma.masked_array(errors_var, [mask[-1,:,:], mask[-1,:,:], mask[-1,:,:]])
+    if save:
+        precip_var[level_idxs,:,:].dump('results/precip_var_' + month)
+        errors_var.dump('results/errors_var_' + month)
+        
+elif approx_type == 'both':
+    for n in range(idx[0].shape[0]):
+        i = idx[0][n]
+        j = idx[1][n]
+        if precip[-1,i,j] != 0:
+            precip_unif[:,i,j] = linear_splines_unif(data=precip[:,i,j], num_knots=10, zero_inflated=True)  
+            precip_var[:,i,j] = linear_splines_var(data=precip[:,i,j], num_knots=10, zero_inflated=True) 
+            if error_calc:
+                errors_unif[:,i,j] = calc_errors(precip[:,i,j], precip_unif[:,i,j])
+                errors_var[:,i,j] = calc_errors(precip[:,i,j], precip_var[:,i,j])
+    precip_unif = np.ma.masked_array(precip_unif, mask)
+    precip_var = np.ma.masked_array(precip_var, mask)
+    errors_unif = np.ma.masked_array(errors_unif, [mask[-1,:,:], mask[-1,:,:], mask[-1,:,:]])
+    errors_var = np.ma.masked_array(errors_var, [mask[-1,:,:], mask[-1,:,:], mask[-1,:,:]])
+    if save:
+        precip_unif[level_idxs,:,:].dump('results/precip_unif_' + month)
+        errors_unif.dump('results/errors_unif_' + month)
+        precip_var[level_idxs,:,:].dump('results/precip_var_' + month)
+        errors_var.dump('results/errors_var_' + month)
+        
 
-# parallel code using multiprocessing - doesn't seem to speed up code with 8 cores though!
-start_time = time.time()
-
-if __name__ == '__main__':
-    if mp.cpu_count() > 16:
-        pool = mp.Pool(processes = mp.cpu_count()-16)
-    else:
-        pool = mp.Pool(processes = np.cpu_count())
-    pool.map_async(wrap, list(range(nonzero_idx[0].shape[0])))
-    pool.close()
-    pool.join()
-
-end_time = time.time()
-print(f'Linear spline code took {end_time - start_time} to run.')
-
-np.save('precip_levels_approx_var_', precip_levels_approx_var)
+# precip_unif = np.load('results/precip_unif_' + month, allow_pickle=True)
+# precip_var = np.load('results/precip_var_' + month, allow_pickle=True)
+# errors_unif = np.load('results/errors_unif_' + month, allow_pickle=True)
+# errors_var = np.load('results/errors_var_' + month, allow_pickle=True) 
