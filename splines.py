1aa3cd3bc8afdaaeb8be53e7b51971f54549a292
Taylor Roper <taylorroper@pop-os.localdomain>
2022-07-13 19:19:40 -0600

    Removed outdated figures. Fixed Nan errors and removed non-monotonic variable node outputs in spline code.

--- splines.py
+++ splines.py
@@ -1,17 +1,18 @@
-
 approx_type = 'both' # 'unif', 'var', or 'both'
 include_temp = False # does not approximate temperature data yet
 error_calc = True
 save = True
 month = 'june'
 include_obs = True
-# months completed: jan, march, april, july
+# months completed: june 
 
 import numpy as np
 import pygrib
 from scipy import optimize
 
 import time
+
+time_start = time.time()
 
 # read in grib file
 
@@ -71,7 +72,7 @@ if include_temp:
     ds_grb.close()
 
 if include_obs:
-    fn_grb = 'urma2p5.2022062612.pcp_06h.wexp.grb2'
+    fn_grb = 'data/urma2p5.2022062612.pcp_06h.wexp.grb2'
     ds_grb = pygrib.open(fn_grb)
     obs = ds_grb.message(1).data()[0]
     ds_grb.close()
@@ -92,7 +93,17 @@ precip = np.ma.masked_array(precip, mask)
 if include_obs:
     obs = np.ma.masked_array(obs, mask[-1,:,:])
 
-# linear spline functions
+
+levels = np.linspace(1,99,99)
+qs = levels / 100
+N = int(1e3)
+if include_obs:
+    data_max = max(obs.max(), precip[-1,:,:].max())
+else:
+    data_max = precip[-1,:,:].max()
+xs = np.linspace(0.0, data_max, N)
+
+# linear spline and error functions
 
 def linear_splines_unif(data, num_knots=10, zero_inflated=True):   
     '''
@@ -102,8 +113,8 @@ def linear_splines_unif(data, num_knots=10, zero_inflated=True):
     ''' 
 
     # checking if cdf is all zero
-    if data[-1] == 0:
-        return np.zeros(99)
+    #if data[-1] == 0:
+    #    return np.zeros(99)
     
     if zero_inflated:
         # calculating where cdf starts being nonzero (all zero cdf's should not be inputted)
@@ -134,39 +145,51 @@ def linear_splines_var(data, num_knots=5, zero_inflated=True):
     knots with optimized placement and returning interpolated approximation at every
     quantile level with level_width.
     '''
-    
-    # checking if cdf is all zero
-    if data[-1] == 0:
-        return np.zeros(99)
 
+    # checking if cdf is all zero
+    #if data[-1] == 0:
+    #    return np.zeros(99)
+    
+    data_ = data # saving full set of data in case optimize.curve_fit fails
+    
+    if zero_inflated:
+        # calculating where cdf starts being nonzero (all zero cdf's should not be inputted)
+        idx_start = max(np.where(data > 0)[0].min() - 1, 0)
+        data = data[idx_start:]
+        if 99-idx_start < num_knots*2:
+            return linear_splines_unif(data_, num_knots=5, zero_inflated=zero_inflated)
+    else:
+        idx_start = 0
+        
     # setting up intial value of parameters
-    p_0 = np.linspace(0,98,num_knots).astype(int)
-    p_0 = np.hstack([data[p_0], p_0])
+    p_0 = np.linspace(idx_start,98,num_knots)
+    p_0 = np.hstack([np.interp(p_0, levels[idx_start:], data), p_0])
 
     # try to fit parameters with RuntimeError exception that returns linear_splines_unif
     # that uses uniformly space knots
     try:
-        fit, _ = optimize.curve_fit(lambda x, *params : linear_splines(x, num_knots, params), np.linspace(1,99,99), data, p_0)
-        levels = range(1,100)
-        return np.interp(levels, fit[num_knots:], fit[:num_knots])
+        fit, _ = optimize.curve_fit(lambda x, *params : linear_splines(x, num_knots, params), 
+                np.array(range(idx_start+1,100)), data, p_0)
+        levels_ = range(idx_start+1,100)
+        for k in range(num_knots-1):
+            if fit[:num_knots][k+1] < fit[:num_knots][k]:
+                return linear_splines_unif(data_, num_knots, zero_inflated)
+        return np.hstack([np.zeros(idx_start), np.interp(levels_, fit[num_knots:], fit[:num_knots])])
     except RuntimeError:
-        return linear_splines_unif(data, num_knots*2, zero_inflated)
-    
-qs = np.linspace(0.01, 0.99, 99)
-N = int(1e3)
+        return linear_splines_unif(data_, num_knots, zero_inflated)
 
 def calc_errors(orig, approx):
-    xs = np.linspace(orig.min(), orig.max(), N)
-    differences = np.abs(np.interp(xs[1:], orig, qs) - np.interp(xs[1:], approx, qs))
-    differences_weighted = differences * (xs[1:] - xs[:-1])
-    return [differences.max(), np.sum(differences_weighted), np.sum(differences * differences_weighted)]
+    if orig[-1] == 0 and approx[-1] == 0:
+        return np.zeros(3)
+    else:
+        differences = np.abs(np.interp(xs[1:-1], orig[1:-1], qs[1:-1], left=0.0, right=1.0) - np.interp(xs[1:-1], approx[1:-1], qs[1:-1], left=0.0, right=1.0))
+        return [differences.max(), np.mean(differences), np.mean(differences**2)] # [KS, mean L_1, CRPS]
 
 def obs_CRPS(obs, approx):
-    xs = np.linspace(approx.min(), approx.max(), N)
-    obs_cdf = np.zeros(xs.shape[0])
-    obs_nonzero = np.where(xs >= obs)[0]
+    obs_cdf = np.zeros(N-2)
+    obs_nonzero = np.where(xs[1:-1] >= obs)[0]
     obs_cdf[obs_nonzero] = np.ones(obs_nonzero.shape[0])
-    return np.sum((np.interp(xs[1:], approx, qs) - obs_cdf[1:])**2 * (xs[1:] - xs[:-1]))
+    return np.mean((np.interp(xs[1:-1], approx[1:-1], qs[1:-1], left=0.0, right=1.0) - obs_cdf)**2)
 
 precip_unif = np.ma.masked_array(np.zeros(precip.shape), mask)
 precip_var = np.ma.masked_array(np.zeros(precip.shape), mask)
@@ -174,7 +197,7 @@ if error_calc:
     errors_unif = np.ma.masked_array(np.zeros((3,) + lat.shape), [mask[-1,:,:],mask[-1,:,:],mask[-1,:,:]])
     errors_var = np.ma.masked_array(np.zeros((3,) + lat.shape), [mask[-1,:,:],mask[-1,:,:],mask[-1,:,:]])    
 if include_obs:
-    obs_crps = np.ma.masked_array(np.zeros((2,)+lat.shape), [mask[-1,:,:],mask[-1,:,:]])
+    obs_crps = np.ma.masked_array(np.zeros((3,)+lat.shape), [mask[-1,:,:],mask[-1,:,:],mask[-1,:,:]])
 idx = np.where(mask[-1,:,:] == 0)
 level_idxs = [4, 24, 50, 74, 94]
 
@@ -183,52 +206,69 @@ if approx_type == 'unif':
         i = idx[0][n]
         j = idx[1][n]
         if precip[-1,i,j] != 0:
-            precip_unif[:,i,j] = linear_splines_unif(data=precip[:,i,j], num_knots=10, zero_inflated=True)  
+            precip_unif[:,i,j] = linear_splines_unif(precip[:,i,j])  
             if error_calc:
                 errors_unif[:,i,j] = calc_errors(precip[:,i,j], precip_unif[:,i,j])
         if include_obs:
-            obs_crps[0,i,j] = obs_CRPS(obs[i,j], precip_unif[:,i,j])
+            obs_crps[0,i,j] = obs_CRPS(obs[i,j], precip[:,i,j])
+            obs_crps[1,i,j] = obs_CRPS(obs[i,j], precip_unif[:,i,j])
     if save:
-        precip_unif[level_idxs,:,:].dump('results/precip_unif_' + month)
-        errors_unif.dump('results/errors_unif_' + month)
+        precip_unif[level_idxs,:,:].dump('results/' + date + '/precip_unif')
+        if error_calc:
+            errors_unif.dump('results/' + date + '/errors_unif')
         if include_obs:
-            obs_crps[0,:,:].dump('results/obs_unif_crps_' + month)
+            obs_crps[0,:,:].dump('results/' + date + '/obs_crps')
+            obs_crps[1,:,:].dump('results/' + date + '/obs_unif_crps')
 
 elif approx_type == 'var':
     for n in range(idx[0].shape[0]):
         i = idx[0][n]
         j = idx[1][n]
         if precip[-1,i,j] != 0:
-            precip_var[:,i,j] = linear_splines_var(data=precip[:,i,j], num_knots=10, zero_inflated=True) 
+            precip_var[:,i,j] = linear_splines_var(precip[:,i,j]) 
             if error_calc:
                 errors_var[:,i,j] = calc_errors(precip[:,i,j], precip_var[:,i,j])
         if include_obs:
-            obs_crps[1,i,j] = obs_CRPS(obs[i,j], precip_var[:,i,j])    
+            obs_crps[0,i,j] = obs_CRPS(obs[i,j], precip[:,i,j])            
+            obs_crps[2,i,j] = obs_CRPS(obs[i,j], precip_var[:,i,j])    
     if save:
-        precip_var[level_idxs,:,:].dump('results/precip_var_' + month)
-        errors_var.dump('results/errors_var_' + month)
+        precip_var[level_idxs,:,:].dump('results/' + date + '/precip_var')
+        if error_calc:
+            errors_var.dump('results/' + date + '/errors_var')
         if include_obs:
-            obs_crps[1,:,:].dump('results/obs_var_crps_' + month)
+            obs_crps[0,:,:].dump('results/' + date + '/obs_crps')            
+            obs_crps[2,:,:].dump('results/' + date + '/obs_var_crps')
 
 elif approx_type == 'both':
     for n in range(idx[0].shape[0]):
         i = idx[0][n]
         j = idx[1][n]
         if precip[-1,i,j] != 0:
-            precip_unif[:,i,j] = linear_splines_unif(data=precip[:,i,j], num_knots=10, zero_inflated=True)  
-            precip_var[:,i,j] = linear_splines_var(data=precip[:,i,j], num_knots=10, zero_inflated=True) 
+            precip_unif[:,i,j] = linear_splines_unif(precip[:,i,j])  
+            precip_var[:,i,j] = linear_splines_var(precip[:,i,j]) 
             if error_calc:
                 errors_unif[:,i,j] = calc_errors(precip[:,i,j], precip_unif[:,i,j])
                 errors_var[:,i,j] = calc_errors(precip[:,i,j], precip_var[:,i,j])
+                if errors_var[1,i,j] > errors_unif[1,i,j]:
+                    precip_var[:,i,j] = precip_unif[:,i,j]
+                    errors_var[:,i,j] = errors_unif[:,i,j]
         if include_obs:
-            obs_crps[0,i,j] = obs_CRPS(obs[i,j], precip_unif[:,i,j])
-            obs_crps[1,i,j] = obs_CRPS(obs[i,j], precip_var[:,i,j])    
+            obs_crps[0,i,j] = obs_CRPS(obs[i,j], precip[:,i,j])            
+            obs_crps[1,i,j] = obs_CRPS(obs[i,j], precip_unif[:,i,j])
+            obs_crps[2,i,j] = obs_CRPS(obs[i,j], precip_var[:,i,j])    
     if save:
-        precip_unif[level_idxs,:,:].dump('results/precip_unif_' + month)
-        errors_unif.dump('results/errors_unif_' + month)
-        precip_var[level_idxs,:,:].dump('results/precip_var_' + month)
-        errors_var.dump('results/errors_var_' + month)
-        obs_crps.dump('results/obs_crps_' + month)
+        precip_unif[level_idxs,:,:].dump('results/' + date + '/precip_unif_')
+        precip_var[level_idxs,:,:].dump('results/' + date + '/precip_var_')
+        if error_calc:
+            errors_unif.dump('results/' + date + '/errors_unif_')
+            errors_var.dump('results/' + date + '/errors_var_')
+        if include_obs:
+            obs_crps[0,:,:].dump('results/' + date + '/obs_crps_')        
+            obs_crps[1,:,:].dump('results/' + date + '/obs_unif_crps_')
+            obs_crps[2,:,:].dump('results/' + date + '/obs_var_crps_')                        
+
+time_end = time.time()
+print(f'Code completed in {(time_end - time_start) / 60 / 60} hours.')
 
 # precip_unif = np.load('results/precip_unif_' + month, allow_pickle=True)
 # precip_var = np.load('results/precip_var_' + month, allow_pickle=True)
